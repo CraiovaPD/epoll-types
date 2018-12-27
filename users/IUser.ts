925deed47ec9c60973f852c935802614657d74ff
Dragos Sebestin <sebestin.dragos@gmail.com>
2018-12-27 13:04:52 +0200

    [master] Added user role field to IUser.

--- users/IUser.ts
+++ users/IUser.ts
@@ -1,6 +1,14 @@
+export enum UserRole {
+  root,
+  admin,
+  moderator,
+  regular
+}
+
 export interface IUser {
   _id: string,
   phone: string,
+  role: UserRole,
   firstname: string,
   lastname?: string
 }
